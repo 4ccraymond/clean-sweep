dc7e741324f9906af42c08297c3124faaf409670
Adeleine Alger <adeleinealger@gmail.com>
2025-04-24 21:44:38 -0600

    Fix imports in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { RouterProvider } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 // import { ChakraProvider } from '@chakra-ui/react';
 
 const router = createBrowserRouter([
