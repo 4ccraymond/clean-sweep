b10f4215c1e05814c9fc7b9fcfe82beab000675e
Adeleine Alger <adeleinealger@gmail.com>
2025-04-24 21:56:05 -0600

    Create pages with routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,11 +5,28 @@ import App from './App'
 import { RouterProvider } from 'react-router-dom'
 import { createBrowserRouter } from 'react-router-dom'
 // import { ChakraProvider } from '@chakra-ui/react';
+import Login from './pages/Login/Login.tsx'
+import Dashboard from './pages/Dashboard/Dashboard.tsx'
+import Signup from './pages/Signup/Signup.tsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>
+    element: <App/>,
+    children: [
+      {
+        index: true,
+        element: <Login/>
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard/>
+      },
+      {
+        path: '/signup',
+        element: <Signup/>
+      }
+    ]
   }
 ]);
 
